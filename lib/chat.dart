6060c3ec89cb8315f8f509afe0bafae713158609
beka-tegen <bekategene177@gmail.com>
2024-02-19 09:25:46 +0300

    modify

--- lib/chat.dart
+++ lib/chat.dart
@@ -8,6 +8,21 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _sendMessage() {
+    _textController.clear();
+    _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,14 +34,16 @@ class _ChatPageState extends State<ChatPage> {
           onPressed: () {},
           color: Colors.white,
         ),
-        title: const Text(
-          '강남스팟',
-          style: TextStyle(color: Colors.white),
-          textAlign: TextAlign.center,
+        title: const Center(
+          child: Text(
+            '강남스팟',
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.menu),
+            icon:const Icon(Icons.menu),
             onPressed: () {},
           ),
         ],
@@ -35,9 +52,13 @@ class _ChatPageState extends State<ChatPage> {
         children: [
           Expanded(
             child: ListView.builder(
+              controller: _scrollController,
               itemCount: 100,
               itemBuilder: (context, index) {
-                return ListTile(
+                return const ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('lib/images/Ellipse 23.png'),
+                  ),
                   title: Text(
                     'title',
                     style: TextStyle(color: Colors.white),
@@ -50,43 +71,57 @@ class _ChatPageState extends State<ChatPage> {
               },
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar:const  BottomAppBar(
-        color: Colors.white30,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              icon: Icon(
-                Icons.add,
-                color: Colors.amber,
-              ),
-              onPressed: null,
-              iconSize: 30,
-            ),
-             Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 25.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder:  OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder:  OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white38),
-                  ),
-                  fillColor:  Color.fromRGBO(238, 238, 238, 1),
-                  filled: true,
-                  hintText: "메세지 보내기",
-                  hintStyle:  TextStyle(
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon:const Icon(
+                    Icons.add,
                     color: Colors.grey,
                   ),
-                  // 
                 ),
-              ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    padding:const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _textController,
+                            decoration:const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "메세지 보내기",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                            style:const TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon:const Icon(
+                            Icons.send,
+                            color: Colors.amber,
+                          ),
+                          onPressed: _sendMessage,
+                          iconSize: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
